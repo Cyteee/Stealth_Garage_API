b952bd49e31c833f73c2c7fd5077b00a1226fd1b
Cyteee <coyotemalandro@gmail.com>
2026-03-24 22:55:52 -0300

    .

--- Stealth-Garage-API/src/main/java/com/SG/Stealth/Garage/API/services/UserService.java
+++ Stealth-Garage-API/src/main/java/com/SG/Stealth/Garage/API/services/UserService.java
@@ -23,4 +23,9 @@ public class UserService {
         Optional<User> obj = repository.findById(id);
         return obj.orElseThrow(() -> new ResourceNotFoundException(id));
     }
+
+    public User insert(User obj) {
+        return repository.save(obj);
+
+    }
 }
