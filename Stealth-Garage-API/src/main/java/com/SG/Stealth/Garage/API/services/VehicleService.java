699f7e9e2542bc08c3d9c4a2815aa865c6b47cbd
Cyteee <coyotemalandro@gmail.com>
2026-04-09 14:49:09 -0300

    mudandas no fromDTO do VehicleService

--- Stealth-Garage-API/src/main/java/com/SG/Stealth/Garage/API/services/VehicleService.java
+++ Stealth-Garage-API/src/main/java/com/SG/Stealth/Garage/API/services/VehicleService.java
@@ -5,6 +5,7 @@ import com.SG.Stealth.Garage.API.controllers.exceptions.DatabaseException;
 import com.SG.Stealth.Garage.API.controllers.exceptions.ResourceNotFoundException;
 import com.SG.Stealth.Garage.API.entities.User;
 import com.SG.Stealth.Garage.API.entities.Vehicle;
+import com.SG.Stealth.Garage.API.repositories.UserRepository;
 import com.SG.Stealth.Garage.API.repositories.VehicleRepository;
 import jakarta.persistence.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -19,26 +20,29 @@ import java.util.Optional;
 public class VehicleService {
 
     @Autowired
-    private VehicleRepository repository;
+    private VehicleRepository vehicleRepository;
+
+    @Autowired
+    private UserRepository userRepository;
 
     public List<Vehicle> findAll() {
-        return repository.findAll();
+        return vehicleRepository.findAll();
     }
 
     public Vehicle findById(Long id) {
-        Optional<Vehicle> obj = repository.findById(id);
+        Optional<Vehicle> obj = vehicleRepository.findById(id);
         return obj.orElseThrow(() -> new ResourceNotFoundException(id));
     }
 
     public Vehicle insert(Vehicle obj){
-        return repository.save(obj);
+        return vehicleRepository.save(obj);
     }
 
     public Vehicle update(Long id, Vehicle obj){
         try {
-            Vehicle entity = repository.getReferenceById(id);
+            Vehicle entity = vehicleRepository.getReferenceById(id);
             updateData(entity, obj);
-            return repository.save(entity);
+            return vehicleRepository.save(entity);
         }catch (EntityNotFoundException e){
             throw new ResourceNotFoundException(id);
         }
@@ -52,7 +56,7 @@ public class VehicleService {
 
     public void delete(Long id){
         try {
-            repository.deleteById(id);
+            vehicleRepository.deleteById(id);
         }catch (EmptyResultDataAccessException e){
             throw new ResourceNotFoundException(id);
         }catch (DataIntegrityViolationException e){
@@ -60,15 +64,11 @@ public class VehicleService {
         }
     }
 
-    public Vehicle fromDTO(VehicleDTO objDto){
-        User owner = new User(
-                objDto.getOwner().getId(),
-                objDto.getOwner().getName(),
-                objDto.getOwner().getEmail(),
-                objDto.getOwner().getPhoneNumber()
-                );
-        return  new Vehicle(
+    public Vehicle fromDTO(VehicleDTO objDto) {
 
+        User owner = userRepository.findById(objDto.getOwner().getId()).orElseThrow(() -> new RuntimeException("Usuário não encontrado!"));
+
+        return new Vehicle(
                 objDto.getId(),
                 objDto.getBrandAndName(),
                 objDto.getYear(),
