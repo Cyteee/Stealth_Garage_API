f54eade46e759466bab16d957e17bf73e59e21a4
Cyteee <coyotemalandro@gmail.com>
2026-04-19 18:47:27 -0300

    Teste unitario com JUnit 5 e Mockito para inserir um veiculo

--- Stealth-Garage-API/src/test/java/com/SG/Stealth/Garage/API/repositories/VehicleRepositoryTest.java
+++ Stealth-Garage-API/src/test/java/com/SG/Stealth/Garage/API/repositories/VehicleRepositoryTest.java
@@ -1,7 +1,9 @@
 package com.SG.Stealth.Garage.API.repositories;
 
 import com.SG.Stealth.Garage.API.entities.Vehicle;
+import com.SG.Stealth.Garage.API.services.VehicleService;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.data.jpa.test.autoconfigure.DataJpaTest;
 import org.springframework.test.context.ActiveProfiles;
@@ -15,14 +17,26 @@ public class VehicleRepositoryTest {
     @Autowired
     private VehicleRepository vehicleRepository;
 
+    @Autowired
+    private VehicleService vehicleService;
+
     @Test
-    void deveSalvarUmVeiculoNoBancoH2(){
-        Vehicle novoVeiculo = new Vehicle();
-        novoVeiculo.setBrandAndName("Volkswagen Tera");
+    void deveInserirUmVeiculoComSucesso() {
+        Vehicle veiculoParaSalvar = new Vehicle();
+        veiculoParaSalvar.setBrandAndName("Honda Civic");
+        veiculoParaSalvar.setYear(2020);
 
-        Vehicle veiculoSalvo = vehicleRepository.save(novoVeiculo);
+        Vehicle veiculoRetornoDoBanco = new Vehicle();
+        veiculoRetornoDoBanco.setId(10L);
+        veiculoRetornoDoBanco.setBrandAndName("Honda Civic");
+        veiculoRetornoDoBanco.setYear(2020);
 
-        assertNotNull(veiculoSalvo.getId(), "O ID nao deverai ser nulo, o banco deveria ter gerado um!");
-        assertEquals("Volkswagen Tera", veiculoSalvo.getBrandAndName());
+        Mockito.when(vehicleRepository.save(Mockito.any(Vehicle.class))).thenReturn(veiculoRetornoDoBanco);
+
+        Vehicle resultado = vehicleService.insert(veiculoParaSalvar);
+
+        assertNotNull(resultado.getId(), "O ID não deveria ser nulo, provando que passou pelo banco falso");
+        assertEquals(10L, resultado.getId());
+        assertEquals("Honda Civic", resultado.getBrandAndName());
     }
 }
