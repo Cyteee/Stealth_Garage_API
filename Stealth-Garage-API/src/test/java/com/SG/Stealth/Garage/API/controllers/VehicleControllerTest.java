d9de4b80e58ad74eeb6c401d883261f397b9143c
Cyteee <coyotemalandro@gmail.com>
2026-04-21 22:34:14 -0300

    Teste unitario com JUnit 5 e Mockito para subir um veiculo pelo ID dele, e retonar com status 200

---
+++ Stealth-Garage-API/src/test/java/com/SG/Stealth/Garage/API/controllers/VehicleControllerTest.java
@@ -0,0 +1,40 @@
+package com.SG.Stealth.Garage.API.controllers;
+
+import com.SG.Stealth.Garage.API.entities.Vehicle;
+import com.SG.Stealth.Garage.API.services.VehicleService;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.webmvc.test.autoconfigure.WebMvcTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+@WebMvcTest(VehicleController.class)
+public class VehicleControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockitoBean
+    private VehicleService vehicleService;
+
+    @Test
+    void deveRetornarVeiculoPeloIdcomStatus200() throws Exception{
+        Long idBuscado = 1L;
+        Vehicle veiculoFalso = new Vehicle();
+        veiculoFalso.setId(idBuscado);
+        veiculoFalso.setBrandAndName("Fiat Marea");
+
+        Mockito.when(vehicleService.findById(idBuscado)).thenReturn(veiculoFalso);
+
+        mockMvc.perform(MockMvcRequestBuilders.get("/vehicle/{id}", idBuscado)
+                .accept(MediaType.APPLICATION_JSON))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.jsonPath("$.id").value(idBuscado))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.brandAndName").value("Fiat Marea"));
+    }
+}
