21eb8155f9196cba584704ce856933212ce2af3a
Cyteee <coyotemalandro@gmail.com>
2026-05-05 17:04:16 -0300

    feat: Implementacao do metodo calculateTotalOrderCost() dentro do OrderService

--- Stealth-Garage-API/src/main/java/com/SG/Stealth/Garage/API/services/OrderService.java
+++ Stealth-Garage-API/src/main/java/com/SG/Stealth/Garage/API/services/OrderService.java
@@ -1,6 +1,23 @@
 package com.SG.Stealth.Garage.API.services;
 
+import com.SG.Stealth.Garage.API.repositories.MaintenceRecordRepository;
+import com.SG.Stealth.Garage.API.strategies.MaintenanceTaskStrategy;
+import com.SG.Stealth.Garage.API.strategies.OilChangeTaskStrategy;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.List;
+
 public class OrderService {
 
+    @Autowired
+    private List<MaintenanceTaskStrategy> tasks;
 
+    public double calculateTotalOrderCost(){
+        double total = 0.0;
+
+        for (MaintenanceTaskStrategy task: tasks){
+            total = total + task.calculateCost();
+        }
+        return total;
+    }
 }
