83f36d2f187bd38d8daa6788c28d3a9f29883d66
Cyteee <coyotemalandro@gmail.com>
2026-05-07 00:31:09 -0300

    refactor: aplicando principios SOLID no layer dos services

--- Stealth-Garage-API/src/test/java/com/SG/Stealth/Garage/API/services/UserServiceTest.java
+++ Stealth-Garage-API/src/test/java/com/SG/Stealth/Garage/API/services/UserServiceTest.java
@@ -1,7 +1,9 @@
 package com.SG.Stealth.Garage.API.services;
 
 import com.SG.Stealth.Garage.API.entities.User;
+import com.SG.Stealth.Garage.API.entities.enums.UserRole;
 import com.SG.Stealth.Garage.API.repositories.UserRepository;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
@@ -12,8 +14,12 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 
 import java.util.List;
 
+import static jdk.internal.classfile.impl.verifier.VerifierImpl.verify;
+import static org.hamcrest.Matchers.any;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
 public class UserServiceTest {
@@ -27,8 +33,37 @@ public class UserServiceTest {
     UserService userService = new UserService(userRepository, passwordEncoder, List.of("admin@teste.com"));
 
     @Test
-    void deve(){
+    void deveAtribuirRoleAdminQuandoEmailEstiverNaLista() {
+        User user = new User();
+        user.setEmail("admin@teste.com");
+        user.setPassword("senha123");
 
+        Mockito.when(passwordEncoder.encode("senha123")).thenReturn("senhaCodificada");
+        Mockito.when(userRepository.save(Mockito.any(User.class))).thenReturn(user);
 
+        User result = userService.insert(user);
+
+        assertNotNull(result);
+        assertEquals("senhaCodificada", result.getPassword());
+
+        Mockito.verify(passwordEncoder, Mockito.times(1)).encode("senha123");
+        Mockito.verify(userRepository, Mockito.times(1)).save(user);
+    }
+
+    @Test
+    void deveAtribuirRoleUserQuandoEmailNaoEstiverNaLista() {
+        User user = new User();
+        user.setEmail("cliente@teste.com");
+        user.setPassword("senha123");
+
+        Mockito.when(passwordEncoder.encode("senha123")).thenReturn("senhaCodificada");
+        Mockito.when(userRepository.save(Mockito.any(User.class))).thenReturn(user);
+
+        User result = userService.insert(user);
+
+        assertNotNull(result);
+
+        Mockito.verify(passwordEncoder, Mockito.times(1)).encode("senha123");
+        Mockito.verify(userRepository, Mockito.times(1)).save(user);
     }
 }
