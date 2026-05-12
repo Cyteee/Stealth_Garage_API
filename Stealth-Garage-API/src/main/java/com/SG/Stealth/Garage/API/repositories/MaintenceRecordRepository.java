82231d7465209d02a375e41dedacfaae19169c51
Cyteee <coyotemalandro@gmail.com>
2026-05-12 17:17:02 -0300

    feat: Implementacao da @Query para buscar todas manutencoes onde o custo ultrapassa um valor teto

--- Stealth-Garage-API/src/main/java/com/SG/Stealth/Garage/API/repositories/MaintenceRecordRepository.java
+++ Stealth-Garage-API/src/main/java/com/SG/Stealth/Garage/API/repositories/MaintenceRecordRepository.java
@@ -2,6 +2,12 @@ package com.SG.Stealth.Garage.API.repositories;
 
 import com.SG.Stealth.Garage.API.entities.MaintenceRecord;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface MaintenceRecordRepository extends JpaRepository<MaintenceRecord, Long> {
+    @Query("SELECT m FROM MaintenanceRecord m WHERE m.cost >= :valorTeto")
+    List<MaintenceRecord> buscarManutencoesAcimaDoTeto(@Param("valorTeto") Double valorTeto);
 }
