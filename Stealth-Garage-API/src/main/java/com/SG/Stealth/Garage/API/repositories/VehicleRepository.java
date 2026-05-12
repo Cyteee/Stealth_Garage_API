c9d5681b48c8647c8b18de438f48ce03e5f34bbc
Cyteee <coyotemalandro@gmail.com>
2026-05-12 16:48:00 -0300

    feat: Implementacao da @Query para buscar todos veiculos pelo ano citado

--- Stealth-Garage-API/src/main/java/com/SG/Stealth/Garage/API/repositories/VehicleRepository.java
+++ Stealth-Garage-API/src/main/java/com/SG/Stealth/Garage/API/repositories/VehicleRepository.java
@@ -8,7 +8,6 @@ import org.springframework.data.repository.query.Param;
 import java.util.List;
 
 public interface VehicleRepository extends JpaRepository<Vehicle, Long> {
-
     @Query("SELECT v FROM Vehicle v WHERE v.year = :ano")
     List<Vehicle> findByYearCustom(@Param("ano") Integer ano);
 }
