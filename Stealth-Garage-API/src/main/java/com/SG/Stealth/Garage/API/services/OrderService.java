f884dfa59c9d23fab3884f11c7758864d1eab58d
Cyteee <coyotemalandro@gmail.com>
2026-05-24 01:27:54 -0300

    fix: anotacao @Service na OrderService

--- Stealth-Garage-API/src/main/java/com/SG/Stealth/Garage/API/services/OrderService.java
+++ Stealth-Garage-API/src/main/java/com/SG/Stealth/Garage/API/services/OrderService.java
@@ -4,9 +4,11 @@ import com.SG.Stealth.Garage.API.repositories.MaintenceRecordRepository;
 import com.SG.Stealth.Garage.API.strategies.MaintenanceTaskStrategy;
 import com.SG.Stealth.Garage.API.strategies.OilChangeTaskStrategy;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+@Service
 public class OrderService {
 
     @Autowired
