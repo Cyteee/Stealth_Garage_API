ca44c8f32da44201b9221566ae9623c0e918aa24
Cyteee <coyotemalandro@gmail.com>
2026-05-24 01:47:46 -0300

    feat: Implementacao do Logger no ResourceExceptionHandler

--- Stealth-Garage-API/src/main/java/com/SG/Stealth/Garage/API/controllers/exceptions/ResourceExceptionHandler.java
+++ Stealth-Garage-API/src/main/java/com/SG/Stealth/Garage/API/controllers/exceptions/ResourceExceptionHandler.java
@@ -5,17 +5,21 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.time.Instant;
 
 @ControllerAdvice
 public class ResourceExceptionHandler {
+    private static final Logger logger = LoggerFactory.getLogger(ResourceExceptionHandler.class);
 
     @ExceptionHandler(ResourceNotFoundException.class)
     public ResponseEntity<StandardError> resourceNotFound(ResourceNotFoundException e, HttpServletRequest request) {
         String error = "Resource not found";
         HttpStatus status = HttpStatus.NOT_FOUND;
         StandardError err = new StandardError(Instant.now(), status.value(), error, e.getMessage(), request.getRequestURI());
+        logger.warn("Tentativa de acesso a recurso inexistente: " + e.getMessage());
         return ResponseEntity.status(status).body(err);
     }
 
@@ -24,6 +28,7 @@ public class ResourceExceptionHandler {
         String error = "Database error";
         HttpStatus status = HttpStatus.BAD_REQUEST;
         StandardError err = new StandardError(Instant.now(), status.value(), error, e.getMessage(), request.getRequestURI());
+        logger.error("Falha de integridade no Banco de Dados: " + e.getMessage());
         return ResponseEntity.status(status).body(err);
     }
 }
