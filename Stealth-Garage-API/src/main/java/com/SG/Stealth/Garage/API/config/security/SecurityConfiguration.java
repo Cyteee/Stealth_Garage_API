0d8596bf756e190d060ce0e62a1f729dfbe66e17
Cyteee <coyotemalandro@gmail.com>
2026-05-25 23:35:46 -0300

    feat: Configurando o CORS para aceitar requisicoes de qualquer origem

--- Stealth-Garage-API/src/main/java/com/SG/Stealth/Garage/API/config/security/SecurityConfiguration.java
+++ Stealth-Garage-API/src/main/java/com/SG/Stealth/Garage/API/config/security/SecurityConfiguration.java
@@ -34,6 +34,7 @@ public class SecurityConfiguration {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
         return http
+                .cors(cors -> cors.configurationSource(corsConfigurationSource()))
                 .csrf(csrf -> csrf.disable())
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(authorize -> authorize
