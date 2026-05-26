67290a7909186e0adc5ff9836eec453c035fa0ff
Cyteee <coyotemalandro@gmail.com>
2026-05-25 23:35:39 -0300

    feat: Configurando o CORS para aceitar requisicoes de qualquer origem

--- Stealth-Garage-API/src/main/java/com/SG/Stealth/Garage/API/config/security/SecurityFilter.java
+++ Stealth-Garage-API/src/main/java/com/SG/Stealth/Garage/API/config/security/SecurityFilter.java
@@ -26,7 +26,6 @@ public class SecurityFilter extends OncePerRequestFilter {
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
         var token = this.recoverToken(request);
-
         if(token != null){
             var email = tokenService.validateToken(token);
             UserDetails user = userRepository.findByEmail(email);
